96175336d3dafb1a3957c46052e29011b083095a
Omaimaelkamaro <oelkamaro@gmail.com>
2024-11-13 14:10:14 +0100

    Initial commi microt

---
+++ discovery/src/test/java/com/example/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DiscoveryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+}
+
