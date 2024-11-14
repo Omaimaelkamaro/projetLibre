4ce332d383f39a9bea68d24ad391d0fbb3da362c
Omaimaelkamaro <oelkamaro@gmail.com>
2024-11-14 13:17:41 +0100

    Con1

--- userAuthentification/src/main/java/com/example/userAuthentification/UserAuthentificationApplication.java
+++ userAuthentification/src/main/java/com/example/userAuthentification/UserAuthentificationApplication.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-@EnableDiscoveryClient
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
 @EntityScan("com.example.userAuthentification.entity")
 @EnableJpaRepositories("com.example.userAuthentification.repository")
 @SpringBootApplication
